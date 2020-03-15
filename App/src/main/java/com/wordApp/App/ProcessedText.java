d7dfa558969fb1561227b2e81fa6de8a4386c2a4
Tom Eriksson <tom.ceriksson@outloutlook.com>
2020-03-15 16:33:52 +0100

    First commit, following spring-boot beginner tutorial

---
+++ App/src/main/java/com/wordApp/App/ProcessedText.java
@@ -0,0 +1,15 @@
+package com.wordApp.App;
+
+public class ProcessedText {
+
+    private final String content;
+
+    public ProcessedText(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
